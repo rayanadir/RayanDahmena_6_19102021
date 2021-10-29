e42ba165c8e34985a96cba3de78f5559f2619154
rayanadir <rayan.dahmena@gmail.com>
2021-10-29 19:01:17 +0200

    filtrage photographeurs fonctionnel

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var tagsArray = [];
+var photographerTemplate;
 
 function selectTag(event) {
     const tag = event.path[0].innerHTML.substring(1);
@@ -37,42 +38,75 @@ fetch('photographers.json').then(res => {
                 document.getElementById('nav').appendChild(li);
             })
             filterPhotographers(photographers);
-
             document.getElementById("photographers").innerHTML = `
-                      ${photographers.map((photographer) =>
-                            photographerTemplate=`
-                      <article class="photographers__photographer">
-                      <a href="/photographer-page/photographer-page.html?id=${photographer.id}">
-                      <img class="photographers__img" src="${photographer.portrait}" alt="${photographer.name}">
-                      </a>
+                              ${photographers.map((photographer) =>
+                                    photographerTemplate=`
+                              <article class="photographers__photographer">
+                              <a href="/photographer-page/photographer-page.html?id=${photographer.id}">
+                              <img class="photographers__img" src="${photographer.portrait}" alt="${photographer.name}">
+                              </a>
 
-                      <h2 class="photographers__name">
-                           ${photographer.name}
-                      </h2>
-                      <p class="photographers__localisation">
-                           ${photographer.city},${photographer.country}
-                      </p>
-                      <p class="photographers__description">
-                           ${photographer.tagline}
-                      </p>
-                      <p class="photographers__price">
-                           ${photographer.price}€ / jour
-                      </p>
-                      <div class="photographers__tags">
-                      ${photographer.tags.map((tag) => `<li class="photographers__tag"> #${tag}</li>`).join('')} 
-                      </div>
-                      </article>
-                      `
-                           
-                           ).join('')}
-                 `
+                              <h2 class="photographers__name">
+                                   ${photographer.name}
+                              </h2>
+                              <p class="photographers__localisation">
+                                   ${photographer.city},${photographer.country}
+                              </p>
+                              <p class="photographers__description">
+                                   ${photographer.tagline}
+                              </p>
+                              <p class="photographers__price">
+                                   ${photographer.price}€ / jour
+                              </p>
+                              <div class="photographers__tags">
+                              ${photographer.tags.map((tag) => `<li class="photographers__tag"> #${tag}</li>`).join('')} 
+                              </div>
+                              </article>
+                              `
+                
+                                   
+                                   ).join('')}
+                         `
+                         console.log(photographerTemplate)
 })
 
+
+
 function filterPhotographers(photographers) {
+    document.getElementById("photographers").innerHTML=``;
+    var photographersArray = [];
     photographers.forEach((photographer) => {
         let isFounded = tagsArray.every(ai => photographer.tags.includes(ai));
         if (isFounded) {
-            console.log(photographer);
+            photographersArray.push(photographer);
         }
-    })
+    });
+    console.log(photographersArray);
+    let photographerList = document.getElementById("photographers");
+        for (var i = 0; i < photographersArray.length; i++) {
+            var template = `
+                <article class="photographers__photographer">
+                                      <a href="/photographer-page/photographer-page.html?id=${photographersArray[i].id}">
+                                      <img class="photographers__img" src="${photographersArray[i].portrait}" alt="${photographersArray[i].name}">
+                                      </a>
+
+                                      <h2 class="photographers__name">
+                                           ${photographersArray[i].name}
+                                      </h2>
+                                      <p class="photographers__localisation">
+                                           ${photographersArray[i].city},${photographers[i].country}
+                                      </p>
+                                      <p class="photographers__description">
+                                           ${photographersArray[i].tagline}
+                                      </p>
+                                      <p class="photographers__price">
+                                           ${photographersArray[i].price}€ / jour
+                                      </p>
+                                      <div class="photographers__tags">
+                                      ${photographersArray[i].tags.map((tag) => `<li class="photographers__tag"> #${tag}</li>`).join('')} 
+                                      </div>
+                                      </article>
+            `
+            photographerList.innerHTML += template;
+        }
 }

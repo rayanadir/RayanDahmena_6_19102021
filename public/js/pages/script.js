6abba6389a6861f571ae2d289aba82af1c557efe
rayanadir <rayan.dahmena@gmail.com>
2021-11-17 18:14:04 +0100

    correction chemin

--- public/js/pages/script.js
+++ public/js/pages/script.js
@@ -6,7 +6,7 @@ var tagsArray = [];
  * @description obtention des données json
  */
 function getData() {
-    fetch('./public/datas/photographers.json')
+    fetch('../public/datas/photographers.json')
         .then(res => {
             return res.json();
         })
